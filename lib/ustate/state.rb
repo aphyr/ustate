b38a1856be5ca56c10ed57b5af381f6aabff58ac
Aphyr <aphyr@aphyr.com>
2011-09-12 17:24:53 -0700

    State: module scope.

--- lib/ustate/state.rb
+++ lib/ustate/state.rb
@@ -1,151 +1,153 @@
-class UState::State
-  include Beefcake::Message
-  
-  optional :time, :int64, 1 
-  optional :state, :string,  2
-  optional :service, :string, 3
-  optional :host, :string, 4
-  optional :description, :string, 5
-  optional :once, :bool, 6
-  optional :metric_f, :float, 15
-
-  # Average a set of states together. Chooses the mean metric, the mode state,
-  # and the mean time. If init is provided, its values override (where present)
-  # the computed ones.
-  def self.average(states, init = State.new)
-    init = case init
-           when State
-             init.dup
-           else
-             State.new init
-           end
+module UState
+  class State
+    include Beefcake::Message
     
-    # Metric
-    init.metric_f ||= states.inject(0.0) { |a, state|
-        a + (state.metric || 0)
-      } / states.size
-    if init.metric_f.nan?
-      init.metric_f = 0.0
+    optional :time, :int64, 1 
+    optional :state, :string,  2
+    optional :service, :string, 3
+    optional :host, :string, 4
+    optional :description, :string, 5
+    optional :once, :bool, 6
+    optional :metric_f, :float, 15
+
+    # Average a set of states together. Chooses the mean metric, the mode state,
+    # and the mean time. If init is provided, its values override (where present)
+    # the computed ones.
+    def self.average(states, init = State.new)
+      init = case init
+             when State
+               init.dup
+             else
+               State.new init
+             end
+      
+      # Metric
+      init.metric_f ||= states.inject(0.0) { |a, state|
+          a + (state.metric || 0)
+        } / states.size
+      if init.metric_f.nan?
+        init.metric_f = 0.0
+      end
+
+      # State
+      init.state ||= states.inject(Hash.new(0)) do |counts, s|
+        counts[s.state] += 1
+        counts
+      end.sort_by { |state, count| count }.last.first rescue nil
+
+      # Time
+      init.time ||= begin
+        states.inject(0) do |a, state|
+          a + state.time.to_f
+        end / states.size
+      rescue
+      end
+      init.time ||= Time.now.to_i
+
+      init
     end
 
-    # State
-    init.state ||= states.inject(Hash.new(0)) do |counts, s|
-      counts[s.state] += 1
-      counts
-    end.sort_by { |state, count| count }.last.first rescue nil
+    # Sum a set of states together. Adds metrics, takes the mode state,
+    # and the mean time. If init is provided, its values override (where present)
+    # the computed ones.
+    def self.sum(states, init = State.new)
+      init = case init
+             when State
+               init.dup
+             else
+               State.new init
+             end
+      
+      # Metric
+      init.metric_f ||= states.inject(0.0) { |a, state|
+          a + (state.metric || 0)
+        }
+      if init.metric_f.nan?
+        init.metric_f = 0.0
+      end
 
-    # Time
-    init.time ||= begin
-      states.inject(0) do |a, state|
-        a + state.time.to_f
-      end / states.size
-    rescue
+      # State
+      init.state ||= states.inject(Hash.new(0)) do |counts, s|
+        counts[s.state] += 1
+        counts
+      end.sort_by { |state, count| count }.last.first rescue nil
+
+      # Time
+      init.time ||= begin
+        states.inject(0) do |a, state|
+          a + state.time.to_f
+        end / states.size
+      rescue 
+      end
+      init.time ||= Time.now.to_i
+
+      init
     end
-    init.time ||= Time.now.to_i
-
-    init
-  end
-
-  # Sum a set of states together. Adds metrics, takes the mode state,
-  # and the mean time. If init is provided, its values override (where present)
-  # the computed ones.
-  def self.sum(states, init = State.new)
-    init = case init
-           when State
-             init.dup
-           else
-             State.new init
-           end
     
-    # Metric
-    init.metric_f ||= states.inject(0.0) { |a, state|
-        a + (state.metric || 0)
-      }
-    if init.metric_f.nan?
-      init.metric_f = 0.0
+    # Finds the maximum of a set of states. Metric is the maximum. State is the
+    # highest, as defined by Dash.config.state_order. Time is the mean.
+    def self.max(states, init = State.new)
+      init = case init
+             when State
+               init.dup
+             else
+               State.new init
+             end
+      
+      # Metric
+      init.metric_f ||= states.inject(0.0) { |a, state|
+          a + (state.metric || 0)
+        }
+      if init.metric_f.nan?
+        init.metric_f = 0.0
+      end
+
+      # State
+      init.state ||= states.inject(nil) do |max, state|
+        state.state if Dash.config[:state_order][state.state] > Dash.config[:state_order][max]
+      end
+
+      # Time
+      init.time ||= begin
+        states.inject(0) { |a, state|
+          a + state.time.to_f
+        } / states.size
+      rescue
+      end
+      init.time ||= Time.now.to_i
+
+      init
     end
 
-    # State
-    init.state ||= states.inject(Hash.new(0)) do |counts, s|
-      counts[s.state] += 1
-      counts
-    end.sort_by { |state, count| count }.last.first rescue nil
-
-    # Time
-    init.time ||= begin
-      states.inject(0) do |a, state|
-        a + state.time.to_f
-      end / states.size
-    rescue 
-    end
-    init.time ||= Time.now.to_i
-
-    init
-  end
-  
-  # Finds the maximum of a set of states. Metric is the maximum. State is the
-  # highest, as defined by Dash.config.state_order. Time is the mean.
-  def self.max(states, init = State.new)
-    init = case init
-           when State
-             init.dup
-           else
-             State.new init
-           end
-    
-    # Metric
-    init.metric_f ||= states.inject(0.0) { |a, state|
-        a + (state.metric || 0)
-      }
-    if init.metric_f.nan?
-      init.metric_f = 0.0
-    end
-
-    # State
-    init.state ||= states.inject(nil) do |max, state|
-      state.state if Dash.config[:state_order][state.state] > Dash.config[:state_order][max]
-    end
-
-    # Time
-    init.time ||= begin
-      states.inject(0) { |a, state|
-        a + state.time.to_f
-      } / states.size
-    rescue
-    end
-    init.time ||= Time.now.to_i
-
-    init
-  end
-
-  # Partition a list of states by a field
-  # Returns a hash of field_value => state
-  def self.partition(states, field)
-    states.inject(Hash.new { [] }) do |p, state|
-      p[state.send(field)] << state
-    end
-  end
-
-  # Sorts states by a field. nil values first.
-  def self.sort(states, field)
-    states.sort do |a, b|
-      a = a.send field
-      b = b.send field
-      if a.nil?
-        -1
-      elsif b.nil?
-        1
-      else
-        a <=> b
+    # Partition a list of states by a field
+    # Returns a hash of field_value => state
+    def self.partition(states, field)
+      states.inject(Hash.new { [] }) do |p, state|
+        p[state.send(field)] << state
       end
     end
-  end
 
-  def metric
-    @metric || metric_f
-  end
+    # Sorts states by a field. nil values first.
+    def self.sort(states, field)
+      states.sort do |a, b|
+        a = a.send field
+        b = b.send field
+        if a.nil?
+          -1
+        elsif b.nil?
+          1
+        else
+          a <=> b
+        end
+      end
+    end
 
-  def metric=(m)
-    @metric = m
+    def metric
+      @metric || metric_f
+    end
+
+    def metric=(m)
+      @metric = m
+    end
   end
 end
