da678acaf45e52c9bef671a2b81ecee9dbb504f5
Kyle Kingsbury <kyle@vodpod.com>
2011-12-21 19:43:08 -0800

    Extract server into ufold.server

--- ufold/test/ufold/test/core.clj
+++ ufold/test/ufold/test/core.clj
@@ -1,28 +1,4 @@
 (ns ufold.test.core
-  (:use [ufold.core])
-  (:use [ufold.client])
-  (:use [clojure.test])
-  (:use [protobuf])
-  (:use [lamina.core]))
+  (:use [ufold.core]))
 
-(deftest echoes
-  (let [server (start-server)
-        client (tcp-client)
-        message (protobuf Msg :foo "hello")]
-    (try
-      (enqueue client (encode message))
-      (is (= message (decode (wait-for-message client))))
-    (finally
-      (close client)
-      (server)))))
 
-(deftest ignores-garbage
-  (let [server (start-server)
-        client (tcp-client)]
-    (try
-      (enqueue client (java.nio.ByteBuffer/wrap (byte-array (map byte [0 1 2]))))
-      (is nil? (wait-for-message client))
-      (is (closed? client))
-      (finally
-        (close client)
-        (server)))))
---
+++ ufold/test/ufold/test/server.clj
@@ -0,0 +1,28 @@
+(ns ufold.test.server
+  (:use [ufold.server])
+  (:use [ufold.client])
+  (:use [clojure.test])
+  (:use [protobuf])
+  (:use [lamina.core]))
+
+(deftest echoes
+  (let [server (tcp-server)
+        client (tcp-client)
+        message (protobuf Msg :foo "hello")]
+    (try
+      (enqueue client (encode message))
+      (is (= message (decode (wait-for-message client))))
+    (finally
+      (close client)
+      (server)))))
+
+(deftest ignores-garbage
+  (let [server (tcp-server)
+        client (tcp-client)]
+    (try
+      (enqueue client (java.nio.ByteBuffer/wrap (byte-array (map byte [0 1 2]))))
+      (is nil? (wait-for-message client))
+      (is (closed? client))
+      (finally
+        (close client)
+        (server)))))
