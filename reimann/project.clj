e54aa754e9de555d5106770cd66cd756ba544b68
aphyr <aphyr@aphyr.com>
2012-02-06 14:22:28 -0800

    Update protobuf to 0.6.0-beta5

--- reimann/project.clj
+++ reimann/project.clj
@@ -6,14 +6,14 @@
   :dependencies [
     [clojure "1.2.0"]
     [aleph "0.2.0"]
-    [protobuf "0.6.0-beta4"]
+    [protobuf "0.6.0-beta5"]
     [org.antlr/antlr "3.2"]
     [com.boundary/high-scale-lib "1.0.3"]
     [clj-time "0.3.4"]
     [com.draines/postal "1.7-SNAPSHOT"]
   ]
   :dev-dependencies [
-    [protobuf "0.6.0-beta4"]
+    [protobuf "0.6.0-beta5"]
   ]
   :test-selectors {:default (complement :integration)
                    :integration :integration
